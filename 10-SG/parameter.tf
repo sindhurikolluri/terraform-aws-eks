85d9c7b9689f5b95d9d1b07e77f973e9fc9c853d
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-04-12 06:35:45 -0400

    daemon set

--- 10-SG/parameter.tf
+++ 10-SG/parameter.tf
@@ -3,34 +3,28 @@ resource "aws_ssm_parameter" "mysql_sg_id" {
   type  = "String"
   value = module.mysql_sg.sg_id
 }
-resource "aws_ssm_parameter" "backend_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/backend_sg_id"
-  type  = "String"
-  value = module.backend_sg.sg_id
-}
 
-resource "aws_ssm_parameter" "frontend_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/frontend_sg_id"
-  type  = "String"
-  value = module.frontend_sg.sg_id
-}
+
 resource "aws_ssm_parameter" "bastion_sg_id" {
   name  = "/${var.project_name}/${var.environment}/bastion_sg_id"
   type  = "String"
   value = module.bastion_sg.sg_id
 }
-resource "aws_ssm_parameter" "app_alb_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/app_alb_sg_id"
+
+resource "aws_ssm_parameter" "alb_ingress_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/alb_ingress_sg_id"
   type  = "String"
-  value = module.app_alb_sg.sg_id
+  value = module.alb_ingress_sg.sg_id
 }
-resource "aws_ssm_parameter" "web_alb_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/web_alb_sg_id"
+
+resource "aws_ssm_parameter" "eks_control_plane_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/eks_control_plane_sg_id"
   type  = "String"
-  value = module.web_alb_sg.sg_id
+  value = module.eks_control_plane_sg.sg_id
 }
-resource "aws_ssm_parameter" "vpn_sg_id" {
-  name  = "/${var.project_name}/${var.environment}/vpn_sg_id"
+
+resource "aws_ssm_parameter" "eks_node_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/eks_node_sg_id"
   type  = "String"
-  value = module.vpn_sg.sg_id
+  value = module.eks_node_sg.sg_id
 }
